82cb316c6308bf419bab4b5bc72ecef4fa1c6951
Fumba <donzo.fumba87@gmail.com>
2022-01-05 15:30:35 +0800

    my personal website update version 4

--- main.py
+++ main.py
@@ -40,14 +40,13 @@ firebase_admin.initialize_app(credential)
 db = firestore.Client()
 """
 #VALIDATE CREDENTIALS & INITIALISATION
-"""
+
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
   'projectId': 'my-website-336004',
 })
 
 db = firestore.client()
-"""
 
 app = Flask(__name__)
 
